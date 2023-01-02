8335a709fbe5eedb40c7a6cb4e2ea468b962fe30
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-01-02 19:49:24 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
