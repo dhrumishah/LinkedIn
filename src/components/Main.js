b4152dbe744748d3d557367c2b479677d77d1ba5
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-01-10 19:17:04 +0530

    created PostCard

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,6 +27,29 @@ const Main = (props) => {
           </button>
         </div>
       </ShareBox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipsis.svg" height={20} />
+            </button>
+          </SharedActor>
+          <Description>Desc</Description>
+          <SharedImg>
+            <a>
+              <img src="/images/shared-img.png" />
+            </a>
+          </SharedImg>
+        </Article>
+      </div>
     </Container>
   );
 };
@@ -99,4 +122,59 @@ const ShareBox = styled(CommonCard)`
     }
   }
 `;
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+`;
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: no-wrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+const Description = styled.div``;
+const SharedImg = styled.div``;
 export default Main;
